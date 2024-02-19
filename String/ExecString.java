485a5a13db95d75a1d8e5d57f6144a68ed60cd5a
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-02-19 15:20:12 -0300

    EX5

--- String/ExecString.java
+++ String/ExecString.java
@@ -1,8 +1,38 @@
 package String;
 
+import java.util.Scanner;
+
 public class ExecString {
-    public static void main(String[] args) {
+
+    String frase1;
+    String frase2;
+    Scanner teclado = new Scanner(System.in);
+
+    public void main(String[] args) {
 
 
+        System.out.println("Escreva a primeira frase: ");
+        setFrase1(teclado.next().toLowerCase());
+        System.out.println("Escreva a segunda frase: ");
+        setFrase2(teclado.next().toLowerCase());
+
+        if(getFrase1().contains(getFrase2())) {
+
+            System.out.println("contem poha");
+        }
+    }
+
+    public String getFrase1() {
+        return frase1;
+    }
+    public void setFrase1(String frase1) {
+        this.frase1 = frase1;
+    }
+    public String getFrase2() {
+        return frase2;
+    }
+    public void setFrase2(String frase2) {
+
+        this.frase2 = frase2;
     }
 }
