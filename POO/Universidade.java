dd078cb1724f2490a9130430abee07512375a67e
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-01 13:10:50 -0300

    POO-3

---
+++ POO/Universidade.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Universidade {
+
+    String nome;
+
+    public Universidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
