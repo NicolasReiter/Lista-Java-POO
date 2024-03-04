f53a69f2b4061349fa9be257c7c5106262a63911
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-04 14:38:31 -0300

    Classe Teste

--- POO/Principal.java
+++ POO/Principal.java
@@ -1,12 +1,22 @@
 package POO;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
         Universidade princenton = new Universidade("Princenton");
         Universidade cambridge = new Universidade("Cambridge");
+
         Departamento juridico = new Departamento("Jurídico");
         Departamento fisica = new Departamento("Física");
+        Departamento economia = new Departamento("Economia");
+        Departamento artes = new Departamento("Artes");
+        Departamento esportes = new Departamento("Esportes");
+        Departamento tecnoligia = new Departamento("Tecnologia");
 
         Pessoa einstein = new Pessoa(14, 3, 1879);
         einstein.setNome("Albert Einstein");
@@ -24,6 +34,24 @@ public class Principal {
         System.out.println(newton);
 
         einstein.setDepartamento(juridico);
-        cambridge.setDepartamento(fisica);
+        newton.setDepartamento(fisica);
+
+        princenton.add(fisica);
+        princenton.add(tecnoligia);
+        cambridge.add(esportes);
+        cambridge.add(juridico);
+        cambridge.add(artes);
+        cambridge.add(economia);
+
+        for (int i = 0; i < princenton.size(); i++) {
+            (princenton.get(i)).setUniversidade(princenton);
+        }
+        for (int i = 0; i < cambridge.size(); i++) {
+            (cambridge.get(i)).setUniversidade(cambridge);
+        }
+
+        if (princenton.size() > 50) {
+            throw new IndexOutOfBoundsException("Máximo de 50 departamentos");
+        }
     }
 }
