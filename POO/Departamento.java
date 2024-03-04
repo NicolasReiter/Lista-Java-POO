5ca5259716f79badabb1f6f28a0f8dfdc2c4d280
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-04 11:12:57 -0300

    POO-4

---
+++ POO/Departamento.java
@@ -0,0 +1,18 @@
+package POO;
+
+public class Departamento {
+
+    String nomeDepartameto;
+
+    public Departamento(String nome) {
+        this.nomeDepartameto = nomeDepartameto;
+    }
+
+    public String getNomeDepartameto() {
+        return nomeDepartameto;
+    }
+
+    public void setNomeDepartameto(String nome) {
+        this.nomeDepartameto = nome;
+    }
+}
