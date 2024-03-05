fb10545718915b74fe4c652d6eac5adb98c2a98c
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-05 16:21:36 -0300

    POO-5

---
+++ POO/Aluno.java
@@ -0,0 +1,30 @@
+package POO;
+
+public class Aluno {
+    String nome;
+    int notaParcial1;
+    int notaParcial2;
+
+    public int getNotaParcial1() {
+        return notaParcial1;
+    }
+
+    public void setNotaParcial1(int notaParcial1) {
+        this.notaParcial1 = notaParcial1;
+    }
+    public int getNotaParcial2() {
+        return notaParcial2;
+    }
+
+    public void setNotaParcial2(int notaParcial2) {
+        this.notaParcial2 = notaParcial2;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
