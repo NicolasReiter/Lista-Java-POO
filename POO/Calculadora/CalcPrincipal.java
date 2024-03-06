6cc96ece98513d53cc958bc181210f615beb74ea
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-06 15:55:49 -0300

    POO-6 Principal

---
+++ POO/Calculadora/CalcPrincipal.java
@@ -0,0 +1,9 @@
+package POO.Calculadora;
+
+public class CalcPrincipal {
+    public static void main(String[] args) {
+        CalcControle calculadora = new CalcControle();
+
+        calculadora.executar();
+    }
+}
