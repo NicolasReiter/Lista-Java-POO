d3d306cfc379539368e80572557c2e15aac32d85
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-06 15:53:33 -0300

    POO-6 Controle

--- POO/Calculadora/CalcControle.java
+++ POO/Calculadora/CalcControle.java
@@ -1,4 +1,49 @@
 package POO.Calculadora;
 
+import java.util.Scanner;
+
 public class CalcControle {
+    CalcDados dados = new CalcDados();
+
+    public void executar() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        dados.setOperando1(teclado.nextDouble());
+
+
+        while (dados.getOperacao() != 's') {
+            System.out.println("Qual operador desejas usar?");
+            dados.setOperacao(teclado.next().charAt(0));
+            System.out.println("Digite o segundo número: ");
+            dados.setOperando2(teclado.nextDouble());
+
+            double resultado = opera(dados.getOperando1(), dados.getOperando2(), dados.getOperacao());
+            dados.setOperando1(resultado);
+            System.out.println("====RESULTADO====");
+            System.out.println(dados.getOperando1());
+            System.out.println("=================");
+        }
+    }
+
+    private double opera(double operando1, double operando2, char operador) {
+        double resultado = 0;
+
+        switch (operador) {
+            case '+':
+                resultado += (operando1 + operando2);
+                break;
+            case '-':
+                resultado += (operando1 - operando2);
+                break;
+            case '*':
+                resultado += (operando1 * operando2);
+                break;
+            case '/':
+                resultado += (operando1 / operando2);
+                break;
+            case 's':
+                System.exit(0);
+        }
+        return resultado;
+    }
 }
