31646c8d03501f40b656739921b2837d9cd813ef
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-07 15:12:19 -0300

    POO-7 Polígono Regular

---
+++ POO/HerancaEPolimorfismo/PolReg.java
@@ -0,0 +1,33 @@
+package POO.HerancaEPolimorfismo;
+
+public class PolReg {
+
+    int numeroLados;
+    int tamanhoLado;
+
+    public PolReg(int numeroLados, int tamanhoLado) {
+        this.numeroLados = numeroLados;
+        this.tamanhoLado = tamanhoLado;
+    }
+
+    public int calculaPerimetro() {
+        int resultado = 0;
+
+        resultado = tamanhoLado * numeroLados;
+        return resultado;
+    }
+
+    public int calculaAngulo() {
+        int resultado = 0;
+        int totalAngulo = 0;
+
+        totalAngulo= (numeroLados - 2) * 180;
+        resultado = totalAngulo / numeroLados;
+        return resultado;
+    }
+
+    public double calculaArea() {
+
+        return 0;
+    }
+}
