6046d3eed81a4265d3c0146e81cb63851e4f50be
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-07 16:47:02 -0300

    POO-6 Interface

---
+++ POO/Relacionamentos/Ex6/CalcInterface.java
@@ -0,0 +1,16 @@
+package POO.Relacionamentos.Ex6;
+
+public class CalcInterface {
+
+    //CLASSE INUTILIZADA!!!
+
+    public double recebeOperando(int i) {
+        return i;
+    }
+    public char recebeOperacao(char operador) {
+        return operador;
+    }
+    public void mostraResultado(double resultado) {
+        System.out.println(resultado);
+    }
+}
