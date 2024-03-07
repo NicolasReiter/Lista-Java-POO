02e7dbc3befc7ef639999fbee6ef4b27ac657bf3
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-07 16:40:10 -0300

    POO-5 Aluno

--- POO/Relacionamentos/Ex5/Aluno.java
+++ POO/Relacionamentos/Ex5/Aluno.java
@@ -8,22 +8,18 @@ public class Aluno {
     public int getNotaParcial1() {
         return notaParcial1;
     }
-
     public void setNotaParcial1(int notaParcial1) {
         this.notaParcial1 = notaParcial1;
     }
     public int getNotaParcial2() {
         return notaParcial2;
     }
-
     public void setNotaParcial2(int notaParcial2) {
         this.notaParcial2 = notaParcial2;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
