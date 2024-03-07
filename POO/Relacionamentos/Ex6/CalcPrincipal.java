c8bae71d7a852dbb329171d6708a46813756a31b
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-07 16:48:00 -0300

    POO-6 Principal

--- POO/Relacionamentos/Ex6/CalcPrincipal.java
+++ POO/Relacionamentos/Ex6/CalcPrincipal.java
@@ -3,7 +3,6 @@ package POO.Relacionamentos.Ex6;
 public class CalcPrincipal {
     public static void main(String[] args) {
         CalcControle calculadora = new CalcControle();
-
         calculadora.executar();
     }
 }
