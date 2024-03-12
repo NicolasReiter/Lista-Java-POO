d715e48d58bae3ba9e672aec55b8c005466875a7
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-12 11:24:18 -0300

    2

---
+++ EstruturasDeDados/Ex2/ListNode.java
@@ -0,0 +1,22 @@
+package EstruturasDeDados.Ex2;
+
+public class ListNode {
+    private String nome;
+    private ListNode proximo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ListNode getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(ListNode proximo) {
+        this.proximo = proximo;
+    }
+}
