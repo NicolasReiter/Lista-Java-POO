9b20d7b91fcea938dd175a9e7be56bc238cfd3a3
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-20 14:12:34 -0300

    Ex2 Controle

--- POO/Lista2/Ex2/Controle.java
+++ POO/Lista2/Ex2/Controle.java
@@ -4,15 +4,18 @@ public class Controle {
     public static void main(String[] args) {
         Ponto2D ponto1 = new Ponto2D();
         Ponto2D ponto2 = new Ponto2D(4, -2);
+        Ponto2D ponto3 = new Ponto2D(ponto2);
 
         System.out.println(ponto1.toString());
         System.out.println(ponto2.toString());
+        System.out.println(ponto3.toString());
 
-        ponto1.movimentaPonto(4, -2);
+        ponto1.movimentaPonto(11, 15);
         System.out.println(ponto1.toString());
 
         System.out.println(ponto1.comparacao(ponto2));
-        System.out.println(ponto1.distancia(ponto2));;
+
+        System.out.println(ponto1.distancia(ponto2));
 
         System.out.println(ponto1.clone());
     }
