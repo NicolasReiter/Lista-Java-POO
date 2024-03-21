20d32c0151b2bcd0d4634dc1130ebc7b23863176
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-21 11:58:29 -0300

    Ex3 Controle

---
+++ POO/Lista2/Ex3/Controle.java
@@ -0,0 +1,34 @@
+package POO.Lista2.Ex3;
+
+public class Controle {
+    public static void main(String[] args) {
+        NumeroComplexo complexoSoma =  new NumeroComplexo(6, 5);
+        NumeroComplexo complexoSoma2 =  new NumeroComplexo(2, -1);
+
+        complexoSoma.somar(complexoSoma2);
+
+        NumeroComplexo complexoSubtrair = new NumeroComplexo(4, 5);
+        NumeroComplexo complexoSubtrair2 = new NumeroComplexo(-1, 3);
+
+        complexoSubtrair.subtrair(complexoSubtrair2);
+
+        NumeroComplexo complexoMultiplica = new NumeroComplexo(5, 1);
+        NumeroComplexo complexoMultiplica2 = new NumeroComplexo(2, -1);
+
+        complexoMultiplica.multiplicar(complexoMultiplica2);
+
+        NumeroComplexo complexoDivide = new NumeroComplexo(1, 5);
+        NumeroComplexo complexoDivide2 = new NumeroComplexo(3, 2);
+
+        complexoDivide.dividir(complexoDivide2);
+
+        NumeroComplexo complexoModulo = new NumeroComplexo(1,3);
+
+        complexoModulo.modulo();
+
+        NumeroComplexo complexoCompara = new NumeroComplexo(2, 2);
+        NumeroComplexo complexoCompara2 = new NumeroComplexo(2, 2);
+
+        System.out.println(complexoCompara.comparacao(complexoCompara2));
+    }
+}
