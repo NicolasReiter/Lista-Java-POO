4e29a6dbe72da51a44538cbdec59ef65e87e9549
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-25 14:47:57 -0300

    Ex4 Reta

--- POO/Lista2/Ex4/Reta.java
+++ POO/Lista2/Ex4/Reta.java
@@ -1,7 +1,6 @@
 package POO.Lista2.Ex4;
 
 public class Reta {
-
     private int pontoX;
     private int pontoY;
 
@@ -10,16 +9,38 @@ public class Reta {
         this.pontoY = pontoY;
     }
 
-    public double angular(Reta reta2) {
+    public double angular(Reta ponto) {
         double angular = 0;
-        angular = ((reta2.getPontoY() - reta2.getPontoX()) / (this.getPontoY()) - this.getPontoX());
+        angular = ((double) (ponto.getPontoY() - this.getPontoY()) / (ponto.getPontoX() - this.getPontoX()));
         return angular;
     }
 
-    public double linear(Reta reta) {
+    public double linear(Reta ponto) {
         double linear = 0;
-        linear = this.getPontoX() * this.angular(reta);
+        double invertido = ((this.getPontoX() * this.angular(ponto)) * (-1));
+        linear = this.getPontoY() + invertido;
         return linear;
+    }
+
+    public boolean possuiPonto(Reta ponto, Reta possui) {
+        double x =  (this.angular(ponto) * possui.getPontoX() + this.linear(ponto));
+        if (Math.round(x) == possui.getPontoY()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void interseccao(Reta pontoB, Reta pontoC, Reta pontoD, Reta intersec) {
+        boolean ab = this.possuiPonto(pontoB, intersec);
+        boolean cd = pontoC.possuiPonto(pontoD, intersec);
+
+        if (ab == true && cd == true) {
+            System.out.println("A intersecção é: X=" + intersec.getPontoX() + " Y=" + intersec.getPontoY());
+        }
+    }
+
+    public String representacao(Reta ponto) {
+        return "(" + this.getPontoX() + ", " + this.getPontoY() + ") (" + ponto.getPontoX() + ", " + ponto.getPontoY() + ")" ;
     }
 
     public int getPontoX() {
