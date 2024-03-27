2f195c5a5b4bfe7ee9dd311eb52cd19f0417cb07
Nicolas Ricardo Reiter <nicolasreiter32@gmail.com>
2024-03-27 11:41:39 -0300

    Ex9 Conjunto

---
+++ POO/Lista2/Ex9/Conjunto.java
@@ -0,0 +1,52 @@
+package POO.Lista2.Ex9;
+
+import java.util.ArrayList;
+
+public class Conjunto extends ArrayList{
+    String elemtento;
+
+    public Conjunto() {
+    }
+
+    public void adiciona(String elemento) {
+        if (this.isEmpty()) {
+            this.add(elemento);
+        } else if(!this.contains(elemento)) {
+            this.add(elemento);
+        }
+    }
+
+    public void pertence(String elemento) {
+        if (this.contains(elemento)) {
+            System.out.println("Conjunto contém o elemento " + elemento);
+        } else {
+            System.out.println("O " + elemento + " não está contido no conjunto!!");
+        }
+    }
+
+    public void uniao(ArrayList outraLista) {
+        for (int i = 0; i < outraLista.size(); i++) {
+            this.adiciona((String) outraLista.get(i));
+        }
+    }
+
+    public ArrayList interseccao(ArrayList outraLista) {
+        ArrayList novaLista = new ArrayList();
+        for (int i = 0; i < outraLista.size(); i++) {
+            if (this.contains(outraLista.get(i))) {
+                novaLista.add(outraLista.get(i));
+            }
+        }
+        return novaLista;
+    }
+
+    public ArrayList menos(ArrayList outraLista) {
+        ArrayList novaLista = new ArrayList();
+        for (int i = 0; i < outraLista.size(); i++) {
+            if (!outraLista.contains(this.get(i))) {
+                novaLista.add(this.get(i));
+            }
+        }
+        return novaLista;
+    }
+}
